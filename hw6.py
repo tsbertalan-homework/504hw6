de15145248aa3b7cdcd06ce4ed22467b418b8f49
Tom S. Bertalan <tom@tombertalan.com>
2012-11-25 18:38:30 -0500

    fixed (?) regimes, and labels

--- hw6.py
+++ hw6.py
@@ -2,8 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sys import exit
-so = (.1, .4)  # example stable-oscilatory parameter combinaiton
-sn = (0.4, .8)  # example stable-nonoscilatory parameter combinaiton
+lc = (.2, .5)  # example limit cycle parameter combinaiton
+sf = (.6, .8)  # example stable focus parameter combinaiton
 #so = (.4, .8)
 #so = (.8, .8)
 minmu = .01
@@ -24,11 +24,13 @@ eig1 = (-B + np.lib.scimath.sqrt(B ** 2 - 4 * A * C)) / 2 / A
 eig2 = (-B - np.lib.scimath.sqrt(B ** 2 - 4 * A * C)) / 2 / A
 
 real = (eig1.imag == 0) & (eig2.imag == 0)
-#real = np.logical_not(real)  # clearly I have made some mistakes
+real = np.logical_not(real)  # clearly I have made some mistakes
 imaginary = np.logical_not(real)
 neg = (eig1.real <= 0) & (eig2.real <= 0)
-negreal = neg | real
-negimag = neg | imaginary
+stable = np.logical_not(neg)  # clearly I have made some mistakes
+unstable = np.logical_not(stable)
+stablefocus = stable | imaginary
+limitcycle = unstable | imaginary
 
 #negimag = neg | imaginary  # I don't understand why | is required here, not &.
                             # the operators seem to be switched (!)
@@ -41,12 +43,12 @@ figuretoactual = lambda figure, maxval: float(figure) / resolution * maxval
 a2f = actualtofigure
 f2a = figuretoactual
 
-fig1 = plt.figure(1, figsize=(16, 8.5))
+fig1 = plt.figure(1, figsize=(11, 8.5))
 numaxes = 3
 numticks = 5
 ax1 = fig1.add_subplot(2, 2, 1)
 ax1.imshow(imaginary, cmap='Greys', origin='lower')
-ax1.set_title('oscilatory \n (Have imaginary eigenvalues.)')
+ax1.set_title('Oscilatory')
 locations = [int(x) for x in np.arange(0, resolution, resolution / numticks)]
 mulabels = [f2a(x, maxmu) for x in locations]
 alabels  = [f2a(x, maxa)  for x in locations]
@@ -59,7 +61,7 @@ ax1.set_ylabel(r'$\alpha$')
 
 ax2 = fig1.add_subplot(2, 2, 2)
 ax2.imshow(neg, cmap='Greys', origin='lower')
-ax2.set_title('stable \n (Eigenvalues have negative real part.)')
+ax2.set_title('Stable')
 locations = [int(x) for x in np.arange(0, resolution, resolution / numticks)]
 alabels = [float(x) / resolution * maxa for x in locations]
 mulabels = [float(x) / resolution * maxmu for x in locations]
@@ -71,9 +73,9 @@ ax2.set_xlabel(r'$\mu$')
 ax2.set_ylabel(r'$\alpha$')
 
 ax3 = fig1.add_subplot(2, 2, 3)
-ax3.imshow(negimag, cmap='Greys', origin='lower')
-ax3.set_title('stable oscilatory \n (Eigenvalues have negative real part and are imaginary.')
-ax3.scatter([a2f(so[0], maxmu)], [a2f(so[1], maxa)], color='k')
+ax3.imshow(limitcycle, cmap='Greys', origin='lower')
+ax3.set_title('Limit Cycle')
+ax3.scatter([a2f(lc[0], maxmu)], [a2f(lc[1], maxa)], color='k')
 ax3.set_xlim([0, resolution])
 ax3.set_ylim([0, resolution])
 locations = [int(x) for x in np.arange(0, resolution, resolution / numticks)]
@@ -87,9 +89,9 @@ ax3.set_xlabel(r'$\mu$')
 ax3.set_ylabel(r'$\alpha$')
 
 ax4 = fig1.add_subplot(2, 2, 4)
-ax4.imshow(negreal, cmap='Greys', origin='lower')
-ax4.set_title('stable non-oscilatory \n (Eigenvalues are negative pure-real.)')
-ax4.scatter([a2f(sn[0], maxmu)], [a2f(sn[1], maxa)], color='k')
+ax4.imshow(stablefocus, cmap='Greys', origin='lower')
+ax4.set_title('Stable Focus')
+ax4.scatter([a2f(sf[0], maxmu)], [a2f(sf[1], maxa)], color='k')
 ax4.set_xlim([0, resolution])
 ax4.set_ylim([0, resolution])
 locations = [int(x) for x in np.arange(0, resolution, resolution / numticks)]
@@ -102,8 +104,9 @@ ax4.set_yticklabels(alabels)
 ax4.set_xlabel(r'$\mu$')
 ax4.set_ylabel(r'$\alpha$')
 
+fig1.suptitle('Problem 1: Regimes')
 plt.tight_layout()
-#fig1.savefig('hw6_1_f1.pdf')
+fig1.savefig('hw6_1_f1.pdf')
 #plt.show()
 #exit()
 ax1es = range(4)
@@ -122,14 +125,14 @@ xl_nullclines = list(np.arange(xmin, xmax, dx))
 
 plt.tight_layout()
 
-for (fignum, parameters, regime) in zip([2, 3], [so, sn], ['Stable Limit Cycle', 'Stable Focus']):
+for (fignum, parameters, regime) in zip([2, 3], [lc, sf], ['Stable Limit Cycle', 'Stable Focus']):
     mu, a = parameters
     figs[fignum] = plt.figure(fignum, figsize=(11, 8.5))
 
     ax1es[fignum] = figs[fignum].add_subplot(2, 1, 1)
     tmin = 0.01
     tmax = 200
-    timeresolution = 4000
+    timeresolution = 8000
     dt = (tmax - tmin) / timeresolution
     tl = list(np.arange(tmin, tmax, dt))
     xp = lambda x, y, mu, a: mu - (1 + a) * x + x ** 2 * y
@@ -168,8 +171,8 @@ for (fignum, parameters, regime) in zip([2, 3], [so, sn], ['Stable Limit Cycle',
     ax2es[fignum].set_title(r'Concentrations over time starting from $x_0=%.1f$, $y_0=%.1f$' % initial)
     ax2es[fignum].set_xlabel(r'$\tau$')
     ax2es[fignum].set_ylabel(r'$x$ or $y$')
-#    figs[fignum].savefig('hw6_1_f2.pdf')
-    figs[fignum].suptitle(regime + r', with the parameters $\mu=%.2f$, $\alpha=%.2f$' % (mu, a))
+    figs[fignum].suptitle('Problem 1: ' + regime + r', with the parameters $\mu=%.2f$, $\alpha=%.2f$' % (mu, a))
+    figs[fignum].savefig('hw6_1_f%i.pdf' % fignum)
 #    plt.tight_layout()
 
 plt.show()
