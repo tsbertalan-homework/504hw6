5329249fa3fadf7c1330e4a749808e053fdc7ab2
Tom S. Bertalan <tom@tombertalan.com>
2012-11-26 02:04:35 -0500

    the two problems have to be run in separate namespaces.

--- hw6.py
+++
@@ -1,4 +0,0 @@
-# https://github.com/tsbertalan/504hw6
-# -*- coding: utf-8 *-*
-import hw6_1
-import hw6_2
---
+++ hw6.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# https://github.com/tsbertalan/504hw6
+python hw6_1
+python hw6_2
